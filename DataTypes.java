8b3439a14c04f37ee82a0e28e3f5a843983e51c3
Sean Guo <seanlguo@gmail.com>
2021-06-10 15:47:07 -0400

    Adding DataTypes.java

--- DataTypes.java
+++ DataTypes.java
@@ -3,6 +3,17 @@ public class DataTypes {
 
 	public static void main(String[] args) {
 		
+		int i = 200;
+		
+		short s = (short) i;
+		System.out.println(s);
+		
+		double d = (double) i;
+		System.out.println(d);
+		
+		byte b = (byte) i;
+		System.out.println(b);
+		
 	}
 
 }
